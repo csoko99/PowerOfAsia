7467318e2f4e606349349938d7aa5cb939eece54
csoko99 <laczkolali77@gmail.com>
2024-08-22 10:07:45 +0200

    last two section almost done

--- navigation.js
+++ navigation.js
@@ -2,7 +2,8 @@ function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
   }
-  function hideSidebar(){
+function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
   }
+  
